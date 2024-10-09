870184e9c198176c1fe64a547448a27c340ccb82
Rory Dunne <31826469+rdunne2020@users.noreply.github.com>
2024-10-08 21:47:32 -0400

    Add tmux-like wezterm bindings and WSL2 support

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -14,8 +14,24 @@ config.font = wezterm.font 'JetBrains Mono'
 
 config.enable_tab_bar = false
 
+-- Update the domain for WSL
+-- config.default_domain = 'WSL:Fedora39'
 
 config.window_background_opacity = 0.8
+
+-- Keybinds
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds=1000}
+config.keys = {
+    { key = '%', mods = 'LEADER|SHIFT', action = wezterm.action.SplitHorizontal{ domain = 'CurrentPaneDomain' }},
+    { key = '"', mods = 'LEADER|SHIFT', action = wezterm.action.SplitVertical{ domain = 'CurrentPaneDomain' }},
+    { key = 'c', mods = 'LEADER', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
+
+    -- Cycle through panes
+    { key = 'LeftArrow', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Left' },
+    { key = 'RightArrow', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Right' },
+    { key = 'UpArrow', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Up' },
+    { key = 'DownArrow', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Down' },
+}
 
 -- and finally, return the configuration to wezterm
 return config
